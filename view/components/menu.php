2a12cd0a43726dc3438f63b6e641654c6ef8c8a9
VMRR <m.ramirez@ignilytic.com>
2022-11-24 16:05:43 -0600

    Se crea menu escritorio

--- view/components/menu.php
+++ view/components/menu.php
@@ -1,7 +1,7 @@
 <!--===============================================
 NAV-MOVIL
 =================================================-->
-<nav class="nav navMovil">
+<nav class="navMovil">
   <div class="navMostar">
     <i class="fa fa-bars menu" id="menu" aria-hidden="true"></i>    
   </div>
@@ -12,5 +12,21 @@ NAV-MOVIL
     <a class="link navMovInterno__link" href="contacto"><h2>Contacto</h2></a>
     <a class="link navMovInterno__link" href="comprar" ><h2>Comprar Boletos</h2></a>
     <a class="link navMovInterno__link" href="cuenta" ><h2>Cuentas</h2></a>      
+  </div>  
+</nav>
+
+<!--===============================================
+NAV DESKTOP
+=================================================-->
+<nav class="navDesktop">  
+  <div class="navCentro">
+    <div class="navCentro__logo"><a class="link navMovInterno__link" href="inicio"><img src="view/src/img/logo.svg" alt="logo"></a></div>
+    <div class="navCentro__items">        
+      <a class="link navMovInterno__link" href="faq"><h2>Preguntas Frecuentes</h2></a>
+      <a class="link navMovInterno__link" href="nosotros"><h2>Nosotros</h2></a>
+      <a class="link navMovInterno__link" href="contacto"><h2>Contacto</h2></a>
+      <a class="link navMovInterno__link" href="comprar" ><h2>Comprar Boletos</h2></a>
+      <a class="link navMovInterno__link" href="cuenta" ><h2>Cuentas</h2></a>     
+    </div>
   </div>  
 </nav>
