8aa948c082d82211ee2f8bbfda8ea4e7102043c7
khuertar <isc.khuertar@gmail.com>
2022-11-28 19:14:01 -0600

    capital se finalizo la pagina de inicio

--- view/components/menu.php
+++ view/components/menu.php
@@ -7,11 +7,11 @@ NAV-MOVIL
   </div>
   <div class="navMovInterno__items">      
     <a class="link navMovInterno__link" href="inicio"><img src="view/src/img/logo.svg" alt="logo"></a>
-    <a class="link navMovInterno__link" href="faq"><h2>Preguntas Frecuentes</h2></a>
-    <a class="link navMovInterno__link" href="nosotros"><h2>Nosotros</h2></a>
-    <a class="link navMovInterno__link" href="contacto"><h2>Contacto</h2></a>
-    <a class="link navMovInterno__link" href="comprar" ><h2>Comprar Boletos</h2></a>
-    <a class="link navMovInterno__link" href="cuenta" ><h2>Cuentas</h2></a>      
+    <a class="link navMovInterno__link" href="#faq"><h2>Preguntas Frecuentes</h2></a>
+    <a class="link navMovInterno__link" href="#nosotros"><h2>Nosotros</h2></a>
+    <a class="link navMovInterno__link" href="#contacto"><h2>Contacto</h2></a>
+    <a class="link navMovInterno__link" href="#comprar" ><h2>Comprar Boletos</h2></a>
+    <a class="link navMovInterno__link" href="#cuenta" ><h2>Cuentas</h2></a>      
   </div>  
 </nav>
 
@@ -22,11 +22,11 @@ NAV DESKTOP
   <div class="navCentro">
     <div class="navCentro__logo"><a class="link navMovInterno__link" href="inicio"><img src="view/src/img/logo.svg" alt="logo"></a></div>
     <div class="navCentro__items">        
-      <a class="link navMovInterno__link" href="faq"><h2>Preguntas Frecuentes</h2></a>
-      <a class="link navMovInterno__link" href="nosotros"><h2>Nosotros</h2></a>
-      <a class="link navMovInterno__link" href="contacto"><h2>Contacto</h2></a>
-      <a class="link navMovInterno__link" href="comprar" ><h2>Comprar Boletos</h2></a>
-      <a class="link navMovInterno__link" href="cuenta" ><h2>Cuentas</h2></a>     
+      <a class="link navMovInterno__link" href="#faq"><h2>Preguntas Frecuentes</h2></a>
+      <a class="link navMovInterno__link" href="#nosotros"><h2>Nosotros</h2></a>
+      <a class="link navMovInterno__link" href="#contacto"><h2>Contacto</h2></a>
+      <a class="link navMovInterno__link" href="#comprar" ><h2>Comprar Boletos</h2></a>
+      <a class="link navMovInterno__link" href="#cuenta" ><h2>Cuentas</h2></a>     
     </div>
   </div>  
 </nav>
